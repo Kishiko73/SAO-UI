4c037fe4fbc1660633d841db930e75ce52ba2944
Kiko <Scarlett.Whyte@Gmail.com>
2020-04-22 00:42:11 +0100

    NoteBlock GUI now highlights the key that is currently being hovered over

--- src/main/java/kishiko73/sao/ui/gui/SAOUINoteBlockScreen.java
+++ src/main/java/kishiko73/sao/ui/gui/SAOUINoteBlockScreen.java
@@ -17,23 +17,25 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class SAOUINoteBlockScreen extends Screen {
 
-    private World world;
-    private BlockPos pos;
-    private BlockState state;
-    private final ResourceLocation keyboard = new ResourceLocation(SAOUIMod.MODID, "textures/keyboard.png");
+    private        final ResourceLocation keyboard  = new ResourceLocation(SAOUIMod.MODID, "textures/keyboard.png");
+    private static final int              keyboardW = 295,
+                                          keyboardH =  85;
+    private              World            world;
+    private              BlockPos         pos;
+    private              BlockState       state;
 
-    private int mouseXBuff, mouseYBuff;
 
     public SAOUINoteBlockScreen(World world, BlockPos pos, BlockState state) {
         super(new TranslationTextComponent("gui.kiko-sao-ui.noteBlockScreen.title"));
         this.world = world;
-        this.pos = pos;
+        this.pos   = pos;
         this.state = state;
     }
 
@@ -56,28 +58,54 @@ public class SAOUINoteBlockScreen extends Screen {
     @Override
     protected void init() {
         super.init();
-        final int keyboardW = 295, keyboardH = 85;
-        addButton(new Button(width - keyboardW >> 1, height - keyboardH >> 1, keyboardW, keyboardH, "",
-                id -> {
-                    Key key = Key.collidingKey(
-                            mouseXBuff - (width -  keyboardW >> 1),
-                            mouseYBuff - (height - keyboardH >> 1)
-                    );
-                    if (key != null)
-                        tune(key.ordinal());
-                    onClose();
-                }) {
-            @Override
-            public void renderButton(int mouseX, int mouseY, float partialTicks) {
-                Minecraft.getInstance().getTextureManager().bindTexture(keyboard);
 
-                RenderSystem.pushMatrix();
-                    mouseXBuff = mouseX;
-                    mouseYBuff = mouseY;
-                    blit(x, y, 0, 0, keyboardW, keyboardH, keyboardW, keyboardH);
-                RenderSystem.popMatrix();
+        Screen instance = this;
+
+        for (Key key : Key.values()) {
+            for (Pair<Pair<Integer, Integer>, Pair<Integer, Integer>> dimension : key.keyType.dimensions) {
+
+                // x   y   width   height   message
+                addButton(new Button(
+                        (width - keyboardW >> 1)  + dimension.getFirst().getFirst() + key.x,
+                        (height - keyboardH >> 1) + dimension.getFirst().getSecond(),
+                        dimension.getSecond().getFirst(),
+                        dimension.getSecond().getSecond(),
+                        "",
+                        id -> {
+                            tune(key.ordinal());
+                            onClose();
+                        }) {
+                    @Override
+                    public void renderButton(int mouseX, int mouseY, float partialTicks) {
+                        Minecraft.getInstance().getTextureManager().bindTexture(keyboard);
+
+                        RenderSystem.pushMatrix();
+                            if (key == Key.collidingKey(
+                                    mouseX - (instance.width -  keyboardW >> 1),
+                                    mouseY - (instance.height - keyboardH >> 1)
+                            )) {
+                                RenderSystem.color3f(
+                                        0.73f,
+                                        1f,
+                                        0.73f
+                                );
+                            } else {
+                                RenderSystem.color3f(
+                                        1f,
+                                        1f,
+                                        1f
+                                );
+                            }
+
+                            blit(x, y, dimension.getFirst().getFirst() + key.x, dimension.getFirst().getSecond(),
+                                    dimension.getSecond().getFirst(),
+                                    dimension.getSecond().getSecond(),
+                                    keyboardW, keyboardH);
+                        RenderSystem.popMatrix();
+                    }
+                });
             }
-        });
+        }
     }
 
     @Override
@@ -95,48 +123,54 @@ public class SAOUINoteBlockScreen extends Screen {
 
     private enum Key {
         F_SHARP_0(  0, KeyType.BLACK),
-        G_0      (  7, KeyType.WHITE_MID),
+        G_0      (  8, KeyType.WHITE_MID),
         G_SHARP_0( 20, KeyType.BLACK),
-        A_0      ( 27, KeyType.WHITE_MID),
+        A_0      ( 28, KeyType.WHITE_MID),
         A_SHARP_0( 40, KeyType.BLACK),
-        B_0      ( 47, KeyType.WHITE_RIGHT),
-        C_0      ( 67, KeyType.WHITE_LEFT),
+        B_0      ( 48, KeyType.WHITE_RIGHT),
+        C_0      ( 68, KeyType.WHITE_LEFT),
         C_SHARP_0( 80, KeyType.BLACK),
-        D_0      ( 87, KeyType.WHITE_MID),
+        D_0      ( 88, KeyType.WHITE_MID),
         D_SHARP_0(100, KeyType.BLACK),
-        E_0      (107, KeyType.WHITE_RIGHT),
-        F_0      (127, KeyType.WHITE_LEFT),
+        E_0      (108, KeyType.WHITE_RIGHT),
+        F_0      (128, KeyType.WHITE_LEFT),
         F_SHARP_1(140, KeyType.BLACK),
-        G_1      (147, KeyType.WHITE_MID),
+        G_1      (148, KeyType.WHITE_MID),
         G_SHARP_1(160, KeyType.BLACK),
-        A_1      (167, KeyType.WHITE_MID),
+        A_1      (168, KeyType.WHITE_MID),
         A_SHARP_1(180, KeyType.BLACK),
-        B_1      (187, KeyType.WHITE_RIGHT),
-        C_1      (207, KeyType.WHITE_LEFT),
+        B_1      (188, KeyType.WHITE_RIGHT),
+        C_1      (208, KeyType.WHITE_LEFT),
         C_SHARP_1(220, KeyType.BLACK),
-        D_1      (227, KeyType.WHITE_MID),
+        D_1      (228, KeyType.WHITE_MID),
         D_SHARP_1(240, KeyType.BLACK),
-        E_1      (247, KeyType.WHITE_RIGHT),
-        F_1      (267, KeyType.WHITE_LEFT),
+        E_1      (248, KeyType.WHITE_RIGHT),
+        F_1      (268, KeyType.WHITE_LEFT),
         F_SHARP_2(280, KeyType.BLACK);
 
         private int x;
         private KeyType keyType;
+        protected boolean isHovered;
 
         Key(int x, KeyType keyType) {
             this.x = x;
             this.keyType = keyType;
+            this.isHovered = false;
         }
 
         public static Key collidingKey(int mouseX, int mouseY) {
+            if (0 > mouseX || mouseX > SAOUINoteBlockScreen.keyboardW ||
+                0 > mouseY || mouseY > SAOUINoteBlockScreen.keyboardH)
+                return null;
+
             int tempX, tempY;
             for (Key key : values()) {
                 for (Pair<Pair<Integer, Integer>, Pair<Integer, Integer>> dimension : key.keyType.dimensions) {
-                    tempX = mouseX - dimension.getFirst().getFirst() - key.x;
-                    tempY = mouseY - dimension.getFirst().getSecond();
-                    if (0 <= tempX && tempX <= dimension.getSecond().getFirst() &&
-                        0 <= tempY && tempY <= dimension.getSecond().getSecond())
-                    return key;
+                    if (0 <= (tempX = mouseX - dimension.getFirst().getFirst() - key.x) &&
+                            tempX <= dimension.getSecond().getFirst() &&
+                        0 <= (tempY = mouseY - dimension.getFirst().getSecond()) &&
+                            tempY <= dimension.getSecond().getSecond())
+                        return key;
                 }
             }
             return null;
@@ -147,16 +181,16 @@ public class SAOUINoteBlockScreen extends Screen {
             new Pair<>(new Pair<>(0, 0), new Pair<>(15, 50))
         )),
         WHITE_LEFT(Arrays.asList(
-            new Pair<>(new Pair<>(0,  0), new Pair<>(12, 50)),
+            new Pair<>(new Pair<>(0,  0), new Pair<>(13, 50)),
             new Pair<>(new Pair<>(0, 50), new Pair<>(20, 35))
         )),
         WHITE_MID(Arrays.asList(
-            new Pair<>(new Pair<>(8,  0), new Pair<>( 5, 50)),
+            new Pair<>(new Pair<>(7,  0), new Pair<>( 6, 50)),
             new Pair<>(new Pair<>(0, 50), new Pair<>(20, 35))
         )),
         WHITE_RIGHT(Arrays.asList(
-                new Pair<>(new Pair<>(8,  0), new Pair<>(12, 50)),
-                new Pair<>(new Pair<>(0, 50), new Pair<>(20, 35))
+            new Pair<>(new Pair<>(7,  0), new Pair<>(14, 50)),
+            new Pair<>(new Pair<>(0, 50), new Pair<>(20, 35))
         ));
 
         //             x        y              dx       dy
